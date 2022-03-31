b69162a79b51633cae02b5ab1656289113a5e0eb
xaviguasch <xaviguasch@gmail.com>
2022-03-31 18:16:56 +0200

    added StartScreen component and fetched quizz questions data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React, { useState, useEffect } from 'react'
+
+import StartScreen from './components/StartScreen'
+
 import './App.css'
 
 function App() {
+  const [isQuizzActive, setIsQuizzActive] = useState(false)
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    console.log('effect ran')
+
+    fetch('https://opentdb.com/api.php?amount=5&category=11&type=multiple')
+      .then((res) => res.json())
+      .then((data) => setQuestions(data.results))
+  }, [])
+
   return (
     <div className='App'>
       <h1>Quizz App</h1>
+
+      {!isQuizzActive && <StartScreen />}
     </div>
   )
 }
---
+++ src/components/StartScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return <div className='StartScreen'>StartScreen</div>
+}
+
+export default StartScreen
